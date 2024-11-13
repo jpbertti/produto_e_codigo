f5aaf3b7f17aca22b03b62a8c323100a7d3c228c
jpbertti <joaobertty@gmail.com>
2024-11-13 11:54:15 -0300

    Adicionar arquivos de projeto.

---
+++ produto_e_codigo/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace produto_e_codigo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblPrecoCusto = new System.Windows.Forms.Label();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSair
+            // 
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnSair.Location = new System.Drawing.Point(16, 194);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(81, 27);
+            this.btnSair.TabIndex = 0;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnCalcular.Location = new System.Drawing.Point(237, 194);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(92, 27);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblPrecoCusto
+            // 
+            this.lblPrecoCusto.AutoSize = true;
+            this.lblPrecoCusto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrecoCusto.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lblPrecoCusto.Location = new System.Drawing.Point(12, 41);
+            this.lblPrecoCusto.Name = "lblPrecoCusto";
+            this.lblPrecoCusto.Size = new System.Drawing.Size(245, 20);
+            this.lblPrecoCusto.TabIndex = 2;
+            this.lblPrecoCusto.Text = "Digite o Valor de um Produto:";
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodigo.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lblCodigo.Location = new System.Drawing.Point(12, 110);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(317, 20);
+            this.lblCodigo.TabIndex = 3;
+            this.lblCodigo.Text = "Digite o Codigo de Origem do Produto:";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(16, 64);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(146, 20);
+            this.txtValor.TabIndex = 4;
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(16, 133);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(146, 20);
+            this.txtCodigo.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 233);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.lblCodigo);
+            this.Controls.Add(this.lblPrecoCusto);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnSair);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblPrecoCusto;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.TextBox txtCodigo;
+    }
+}
+
---
+++ produto_e_codigo/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace produto_e_codigo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja realmente sair?", "Sair", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                Close();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double valor;
+            int codigo;
+
+            valor = double.Parse(txtValor.Text);
+            codigo = int.Parse(txtCodigo.Text);
+
+            if (codigo == 1)
+            {
+                MessageBox.Show("Valor do Produto: " + valor + "\nRegião Sul");
+            }
+
+            else if (codigo == 2)
+            {
+                MessageBox.Show("Valor do Produto: " + valor + "\nRegião Norte");
+            }
+
+            else if (codigo == 3)
+            {
+                MessageBox.Show("Valor do Produto: " + valor + "\nRegião Leste");
+            }
+
+            else if (codigo == 4)
+            {
+                MessageBox.Show("Valor do Produto: " + valor + "\nRegião Oeste");
+            }
+
+            else if (codigo == 5)
+            {
+                MessageBox.Show("Valor do Produto: " + valor + "\nRegião Nordeste ");
+            }
+
+            else if (codigo == 6)
+            {
+                MessageBox.Show("Valor do Produto: " + valor + "\nRegião Nordeste ");
+            }
+
+            else if (codigo == 7)
+            {
+                MessageBox.Show("Valor do Produto: " + valor + "\nRegião Centro-Oeste");
+            }
+
+            else if (codigo == 8)
+            {
+                MessageBox.Show("Valor do Produto: " + valor + "\nRegião Centro-Oeste");
+            }
+
+            else
+            {
+                MessageBox.Show("Valor do Produto: " + valor + "\nImportado");
+            }
+        }
+    }
+}
+    
